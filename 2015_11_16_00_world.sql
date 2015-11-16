2498cf4339ab56a3f812802cc3aa4f1b8de15761
geshanet <ufo-x@mail.ru>
2015-11-16 21:03:46 +0700

    Create 2015_11_16_00_world.sql
    remove junk quest 1641

---
+++ 2015_11_16_00_world.sql
@@ -0,0 +1,5 @@
+DELETE FROM `quest_template` WHERE (ID = 1641);
+DELETE FROM `quest_template_addon` WHERE (ID = 1641);
+DELETE FROM `quest_request_items` WHERE (ID = 1641);
+DELETE FROM `quest_offer_reward` WHERE (ID = 1641);
+DELETE FROM `creature_questender` WHERE (quest = 1641);
